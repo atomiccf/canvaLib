478d84a14f939a87ce6b39f41fa34f6e5fffa6c9
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2024-04-08 15:19:44 +0200

    add conditions

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -5,13 +5,13 @@ import React from "react";
 
 export const Canvas = () => {
 
-      const canvas = useRef<HTMLCanvasElement>(null)
+    const canvas = useRef<HTMLCanvasElement>(null)
 
 
-      const [isDrawing, setIsDrawing] = useState(false)
-      const [x, setX] = useState<number>(0)
-      const [y, setY] = useState<number>(0)
-      const tab = document.getElementById('tabs')
+    const [isDrawing, setIsDrawing] = useState(false)
+    const [x, setX] = useState<number>(0)
+    const [y, setY] = useState<number>(0)
+    const tab = document.getElementById('tabs')
     useEffect(() => {
 
         const context = canvas.current!.getContext('2d')
@@ -19,47 +19,49 @@ export const Canvas = () => {
 
         const canvasX = Math.round(canvasOffset?.left); // Subtract the 'left' of the canvas
 
-      // @ts-ignore
-      const mouseDown = (e) => {
-          const canvasX = Math.round(e.clientX - canvasOffset?.left); // Subtract the 'left' of the canvas
-          const canvasY = Math.round(e.clientY - canvasOffset?.top);
+        // @ts-ignore
+        const mouseDown = (e) => {
+            const canvasX = Math.round(e.clientX - canvasOffset?.left); // Subtract the 'left' of the canvas
+            const canvasY = Math.round(e.clientY - canvasOffset?.top);
 
-          setX(canvasX);
-          setY(canvasY);
-          setIsDrawing(true);
-      }
+            setX(canvasX);
+            setY(canvasY);
+            setIsDrawing(true);
+        }
 
-      // @ts-ignore
-      const mouseMove = (e) => {
-          if (!isDrawing) return;
-          const newX = Math.round(e.clientX - canvasOffset?.left); // Subtract the 'left' of the canvas
-          const newY = Math.round(e.clientY - canvasOffset?.top);
-          const width = newX - x
-          const height = newY - y
-          drawRect(context,x,y, width, height)
+        // @ts-ignore
+        const mouseMove = (e) => {
+            if (!isDrawing) return;
+            const newX = Math.round(e.clientX - canvasOffset?.left); // Subtract the 'left' of the canvas
+            const newY = Math.round(e.clientY - canvasOffset?.top);
+            const width = newX - x
+            const height = newY - y
+            drawRect(context, x, y, width, height)
 
-          setX(canvasX);
-          setY(canvasX);
-      }
-      // @ts-ignore
-      const mouseUp = () => {
-          if (!isDrawing) return;
-          setIsDrawing(false);
-      }
+            setX(canvasX);
+            setY(canvasX);
+        }
+        // @ts-ignore
+        const mouseUp = () => {
+            if (!isDrawing) return;
+            setIsDrawing(false);
+        }
 
-         // @ts-ignore
-        canvas?.current.addEventListener('mousedown', mouseDown);
-          // @ts-ignore
-        canvas?.current.addEventListener('mousemove', mouseMove);
-          window.addEventListener("mouseup", mouseUp);
-          return () => {
-              // @ts-ignore
-              canvas?.current.removeEventListener('mousedown', mouseDown)
-              // @ts-ignore
-              canvas?.current.removeEventListener('mousemove', mouseMove)
-              window.removeEventListener("mouseup", mouseUp)
-          }
-      })
+        if (canvas && canvas.current) {
+            canvas?.current.addEventListener('mousedown', mouseDown);
+            canvas?.current.addEventListener('mousemove', mouseMove);
+
+        }
+        window.addEventListener("mouseup", mouseUp);
+        return () => {
+            // @ts-ignore
+            if (canvas && canvas.current) {
+                canvas.current.removeEventListener('mousedown', mouseDown);
+                canvas.current.removeEventListener('mousemove', mouseMove);
+            }
+            window.removeEventListener("mouseup", mouseUp)
+        }
+    })
 
     return (
         <canvas id="canvas" width={tab?.offsetWidth} height="300px" ref={canvas!}></canvas>
